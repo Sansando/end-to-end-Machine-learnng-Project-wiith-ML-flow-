5e8594f7cb960af7fc9c74feb8392a9b81505afe
sanson <sansonsando20@gmail.com>
2025-05-19 20:06:26 +0530

    exeception,logger & utils added

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "sanson"
+AUTHOR_USER_NAME = "sanso"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "sansonsndo20@gmail.com"
+AUTHOR_EMAIL = "sansonsando20@gmail.com"
 
 
 setuptools.setup(
