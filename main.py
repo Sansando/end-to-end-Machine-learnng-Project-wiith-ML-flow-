0d3630aaa5ffda431c97912e9aaa8400e22d3394
sanson <sansonsando20@gmail.com>
2025-05-21 22:15:11 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline    
 
-logger.info("Welcome to our coustom logger")
-
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx====================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
